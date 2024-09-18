c6e38458b5df4bda943164fba237da9014b9e74d
ali72-20 <as20201118@gmail.com>
2024-09-18 16:32:21 +0300

    create task controller

---
+++ src/main/java/com/example/task_managment_server/controllers/TaskController.java
@@ -0,0 +1,40 @@
+package com.example.task_managment_server.controllers;
+import com.example.task_managment_server.Entities.Task;
+import com.example.task_managment_server.Services.TaskServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/tasks")
+public class TaskController {
+    @Autowired
+    private TaskServices taskService;
+
+    @GetMapping
+    public ResponseEntity<List<Task>> getTasks(@RequestParam Optional<String> status) {
+        List<Task> tasks = taskService.getTasks(status);
+        return ResponseEntity.ok(tasks);
+    }
+
+    @PostMapping
+    public ResponseEntity<Task> createTask(@RequestBody Task task) {
+        Task createdTask = taskService.createTask(task);
+        return ResponseEntity.ok(createdTask);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody Task task) {
+        Task updatedTask = taskService.editTask(id, task);
+        return ResponseEntity.ok(updatedTask);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
+        taskService.deleteTask(id);
+        return ResponseEntity.noContent().build();
+    }
+}
