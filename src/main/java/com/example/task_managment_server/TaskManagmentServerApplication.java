0a06c72eeeca7e6fef66e22ff56a6bf88902f8bd
ali72-20 <as20201118@gmail.com>
2024-09-18 16:04:30 +0300

    create a new project managment server

---
+++ src/main/java/com/example/task_managment_server/TaskManagmentServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.task_managment_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagmentServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagmentServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/task_managment_server/TaskManagmentServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.task_managment_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagmentServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
